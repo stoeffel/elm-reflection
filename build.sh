3fe0e9f7dd758d00fa75daef5dc13e3b67dbb937
Stoeffel <schtoeffel@gmail.com>
2016-12-16 18:13:35 +0100

    don't run build in prepublish

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 stack build
-mv .stack-work/install/x86_64-osx/lts-7.13/8.0.1/bin/elm-reflection bin/elm-reflection
+cp .stack-work/install/x86_64-osx/lts-7.13/8.0.1/bin/elm-reflection bin/elm-reflection
